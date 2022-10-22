365932624ca58c93707a5e2497ae4a4b565fcadf
shon-arch <sunnywahane14@gmail.com>
2022-10-22 16:24:27 +0530

    Updated the home page design

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -2,16 +2,28 @@ import './Intro.css'
 
 export const Intro = () => {
     return <div className="intro">
-            <div className='main-content'>
-            <div className="intro-name">
-                Sunny Wahane
-            </div>
-            <div className="description">
-                soFTWARE enGIneeR
-            </div>
+            <div className="content-container">
+                <div className='main-content'>
+                    <h1>
+                    Sunny Wahane
+                    </h1>
+                    <div className="description">
+                        soFTWARE enGIneeR
+                    </div>
+                </div>
+                <div className='about'>
+                    <div className='about-heading'>
+                        About Me
+                    </div>
+                    <div className='about-description'>
+                        Hi, I am a full stack developer based in India
+                        who loves to solve complex problems.  
+                        <br /> 
+                        Apart from development I have keen interest in alogrithms and table tennis
+                    </div>
+                </div>
             </div>
             <div className='down-arrow'>
-                { '>' }
             </div>
         </div>
 }
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,17 +1,14 @@
 import { NavItem } from "../NavItem/NavItem"
-import { NavLogo } from "../NavLogo/NavLogo"
 import './Nav.css'
 
 export const Nav = () => {
     return <nav className="navbar">
-        {/* <div className="nav-logo">
-            <NavLogo />
-        </div> */}
         <div className="nav-items">
-            <NavItem value="home"/>
-            <NavItem value="expertise"/>
-            <NavItem value="work"/>
-            <NavItem value="contact"/>
+            <NavItem value="Home"/>
+            <NavItem value="Projects"/>
+            <NavItem value="Work"/>
+            <NavItem value="Contact"/>
+            <NavItem value="Resume"/>
         </div>
     </nav>
 }
