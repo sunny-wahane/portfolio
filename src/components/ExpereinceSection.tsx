b45239c27d5e47037ba29a27232ad9a963308154
sunnyonecode <120376299+sunnyonecode@users.noreply.github.com>
2023-06-07 11:33:30 +0530

    master| refactored code

---
+++ src/components/ExpereinceSection.tsx
@@ -0,0 +1,18 @@
+export default function ExpeirenceSection({time, brand, description}: {time: string, brand: string, description: string[]}) {
+    return (
+        <li className="ml-5 mb-10">
+                        <div className="absolute w-4 h-4 bg-primary rounded-full mt-2 -left-2 border border-primary"></div>
+                        <h6 className="font-heading p-1 text-2xl"></h6>
+                        <h4 className="font-heading p-1 text-3xl">ZET</h4>
+                            <ul className="list-disc font-primary pl-5 py-2">
+                                {
+                                    description.map(item => {
+                                        return <li className="py-1">{item}</li>
+
+                                    })
+                                }
+                            </ul>
+                        <button className="cursor-none px-5 py-1 mt-5 ml-2 hover:bg-primary hover:text-white font-primary rounded-md border-2 border-primary"><a href="" className="cursor-none">Learn More</a></button>
+        </li>
+    )
+}
--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -1,45 +1,30 @@
+import ExpeirenceSection from "./ExpereinceSection"
+
 export default function WorkExperience() {
+
+    const zetDescription: string[] = [
+        "As an engineer, I am working on Communication Platform to set up user engagement for medly by enabling messaging through push notifications, SMS, and emails",
+        "Worked on integrating Braze into Communication platform.",
+        "Documented different features of Braze such as Multichannel-Campaigns, Canvas, API Campaigns etc. Worked on AWS Kinesis for emitting event from communication service for other services.",
+        "Created API endpoints to store user communication preferences coming from different B2C apps."
+    ]
+
+    const infineonDescription: string[] = [
+        "As an Intern I am working in collaboration with different teams to Enable Model Based System Engineering at Infineon",
+        "Built a Plugin in Enterprise architect to Upload Diagrams and Elements such as Requirements, Use Case Analysis, Block Definition, Activity etc. from Enterprise architect to Jama through REST API",
+        "Created Wrappers around Jama APIs.",
+        "Created Rest Api and routes in their in house framework to Upload the Data coming from the plugin through the wrapper functions."
+    ]
+
+
     return (
         <div>
             <h2 className="font-heading text-5xl ml-20 pl-10 pb-16">01. Expeirence </h2>
             <div className="relative border-l pl-5 border-primary w-9/12 mx-auto">
                 <ol>
-                    <li className="ml-5 mb-10">
-                        <div className="absolute w-4 h-4 bg-primary rounded-full mt-2 -left-2 border border-primary"></div>
-                        <h6 className="font-heading p-1 text-2xl">December 2022 - Present</h6>
-                        <h4 className="font-heading p-1 text-3xl">ZET</h4>
-                            <ul className="list-disc font-primary pl-5 py-2">
-                            <li className="py-1">As an engineer, I am working on Communication Platform to set up user engagement for medly by enabling messaging through push notifications, SMS, and emails.</li>
-                            <li className="py-1">Worked on integrating Braze into Communication platform.</li>
-                            <li className="py-1">Documented different features of Braze such as Multichannel-Campaigns, Canvas, API Campaigns etc. Worked on AWS Kinesis for emitting event from communication service for other services.</li>
-                            <li className="py-1">Created API endpoints to store user communication preferences coming from different B2C apps. Technologies Used: Kotlin, Micronaut, Kotest, Mockk, PostgreSQL, AWS</li>
-                            </ul>
-                        <button className="cursor-none px-5 py-1 mt-5 ml-2 hover:bg-primary hover:text-white font-primary rounded-md border-2 border-primary"><a href="" className="cursor-none">Learn More</a></button>
-                    </li>
-                    <li className="ml-5 mb-10">
-                        <div className="absolute w-4 h-4 bg-primary rounded-full mt-2 -left-2 border border-primary"></div>
-                        <h6 className="font-heading p-1 text-2xl">May 2022 - December 2022</h6>
-                        <h4 className="font-heading p-1 text-3xl">Medly</h4>
-                            <ul className="list-disc font-primary pl-5 py-2">
-                            <li className="py-1">As an engineer, I am working on Communication Platform to set up user engagement for medly by enabling messaging through push notifications, SMS, and emails.</li>
-                            <li className="py-1">Worked on integrating Braze into Communication platform.</li>
-                            <li className="py-1">Documented different features of Braze such as Multichannel-Campaigns, Canvas, API Campaigns etc. Worked on AWS Kinesis for emitting event from communication service for other services.</li>
-                            <li className="py-1">Created API endpoints to store user communication preferences coming from different B2C apps. Technologies Used: Kotlin, Micronaut, Kotest, Mockk, PostgreSQL, AWS</li>
-                            </ul>
-                        <button className="cursor-none px-5 py-1 mt-5 ml-2 hover:bg-primary hover:text-white    font-primary rounded-md border-2 border-primary"><a href="" className="cursor-none">Learn More</a></button>
-                    </li>
-                    <li className="ml-5 mb-10">
-                        <div className="absolute w-4 h-4 bg-primary rounded-full mt-2 -left-2 border border-primary"></div>
-                        <h6 className="font-heading p-1 text-2xl">June 2021 - April 2022</h6>
-                        <h4 className="font-heading p-1 text-3xl">Infineon</h4>
-                            <ul className="list-disc font-primary pl-5 py-2">
-                            <li className="py-1">As an engineer, I am working on Communication Platform to set up user engagement for medly by enabling messaging through push notifications, SMS, and emails.</li>
-                            <li className="py-1">Worked on integrating Braze into Communication platform.</li>
-                            <li className="py-1">Documented different features of Braze such as Multichannel-Campaigns, Canvas, API Campaigns etc. Worked on AWS Kinesis for emitting event from communication service for other services.</li>
-                            <li className="py-1">Created API endpoints to store user communication preferences coming from different B2C apps. Technologies Used: Kotlin, Micronaut, Kotest, Mockk, PostgreSQL, AWS</li>
-                            </ul>
-                        <button className="cursor-none px-5 py-1 mt-5 ml-2 hover:bg-primary hover:text-white  font-primary rounded-md border-2 border-primary"><a href="" className="cursor-none">Learn More</a></button>
-                    </li>
+                    <ExpeirenceSection time = "December 2022 - Present" brand ="ZET" description={zetDescription}/>
+                    <ExpeirenceSection time = "May 2022 - December 2022" brand="Medly" description={infineonDescription}/>
+                    <ExpeirenceSection time = "June 2021 - April 2022" brand="Infineon" description={infineonDescription}/>
                 </ol>
             </div>
             </div>
